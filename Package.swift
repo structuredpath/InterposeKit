7c4f58f27566652a77bc2bdc60ac88f06ee1cab3
Lukas Kubanek <lukas.kubanek@me.com>
2025-03-22 19:00:09 +0100

    Cleaned up Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,30 @@
 // swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "InterposeKit",
     platforms: [
-       .iOS(.v11),
-       .macOS(.v10_13),
-       .tvOS(.v11),
-       .watchOS(.v5)
-     ],
+        .iOS(.v11),
+        .macOS(.v10_13),
+        .tvOS(.v11),
+        .watchOS(.v5)
+    ],
     products: [
-        .library(name: "InterposeKit", targets: ["InterposeKit"]),
+        .library(
+            name: "InterposeKit",
+            targets: ["InterposeKit"]
+        ),
     ],
     targets: [
         .target(name: "SuperBuilder"),
-        .target(name: "InterposeKit", dependencies: ["SuperBuilder"]),
-        .testTarget(name: "InterposeKitTests", dependencies: ["InterposeKit"]),
+        .target(
+            name: "InterposeKit",
+            dependencies: ["SuperBuilder"]
+        ),
+        .testTarget(
+            name: "InterposeKitTests",
+            dependencies: ["InterposeKit"]
+        ),
     ]
 )
