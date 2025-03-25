fd6901ece77c788e4e063b05ccfdee9af0abe98a
Lukas Kubanek <lukas.kubanek@me.com>
2025-03-25 22:31:43 +0100

    First take on ClassHookStrategy

--- Sources/InterposeKit/Hooks/HookStrategy.swift
+++ Sources/InterposeKit/Hooks/HookStrategy.swift
@@ -1,15 +1,22 @@
 import ObjectiveC
 
-final class AnyHookStrategy {
+protocol HookStrategy: AnyObject {
+    
+    /// The replacement implementation used to interpose the method, created during hook setup.
+    var replacementIMP: IMP { get }
+    
+    /// The original method implementation, captured when the hook is applied.
+    var originalIMP: IMP? { get set }
+    
+}
+
+final class AnyHookStrategy: HookStrategy {
     
     init(replacementIMP: IMP) {
         self.replacementIMP = replacementIMP
     }
     
-    /// The replacement implementation used to interpose the method, created during hook setup.
     let replacementIMP: IMP
-    
-    /// The original method implementation, captured when the hook is applied.
     var originalIMP: IMP?
     
 }
