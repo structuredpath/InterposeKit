28ab2ac68d932aca431bc11bf8a96a8efdc668ee
Lukas Kubanek <lukas.kubanek@me.com>
2025-03-26 19:07:01 +0100

    `HookStrategy.lookUpIMP()`

--- Sources/InterposeKit/Hooks/HookStrategy/HookStrategy.swift
+++ Sources/InterposeKit/Hooks/HookStrategy/HookStrategy.swift
@@ -2,7 +2,13 @@ import ObjectiveC
 
 // TODO: Make originalIMP non-optional
 
-protocol HookStrategy: AnyObject, CustomDebugStringConvertible {
+internal protocol HookStrategy: AnyObject, CustomDebugStringConvertible {
+    
+    /// The class whose instance method is being interposed.
+    var `class`: AnyClass { get }
+    
+    /// /// The selector identifying the instance method being interposed.
+    var selector: Selector { get }
     
     /// The implementation used to interpose the method, created during hook setup and used
     /// to replace the original implementation while the hook is applied.
@@ -15,5 +21,25 @@ protocol HookStrategy: AnyObject, CustomDebugStringConvertible {
     func replaceImplementation() throws
     
     func restoreImplementation() throws
+    
+}
+
+extension HookStrategy {
+    
+    /// Dynamically resolves the current IMP of the hooked method by walking the class hierarchy.
+    /// This may return either the original or a hook IMP, depending on the state of the hook.
+    internal func lookUpIMP() -> IMP? {
+        var currentClass: AnyClass? = self.class
+        
+        while let `class` = currentClass {
+            if let method = class_getInstanceMethod(`class`, self.selector) {
+                return method_getImplementation(method)
+            } else {
+                currentClass = class_getSuperclass(currentClass)
+            }
+        }
+        
+        return nil
+    }
     
 }
