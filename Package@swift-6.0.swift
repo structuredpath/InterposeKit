c9d2f82221a588997ad854936377762e177ed84a
Lukas Kubanek <lukas.kubanek@me.com>
2025-04-04 13:28:54 +0200

    Added package manifest specific to Swift 6

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -26,5 +26,6 @@ let package = Package(
             name: "InterposeKitTests",
             dependencies: ["InterposeKit"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
