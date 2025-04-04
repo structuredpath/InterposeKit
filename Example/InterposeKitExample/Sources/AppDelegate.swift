6aa695dcea299c6a56f7f654259e67f7bedd8f16
Lukas Kubanek <lukas.kubanek@me.com>
2025-04-04 15:49:40 +0200

    Added initial window content

--- Example/InterposeKitExample/Sources/AppDelegate.swift
+++ Example/InterposeKitExample/Sources/AppDelegate.swift
@@ -1,8 +1,47 @@
 import AppKit
+import SwiftUI
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    private lazy var window: NSWindow = {
+        let window = NSWindow(
+            contentRect: .zero,
+            styleMask: [.titled, .closable, .miniaturizable],
+            backing: .buffered,
+            defer: false
+        )
+        
+        window.title = "InterposeKit Example"
+        window.contentViewController = self.hostingController
+        
+        return window
+    }()
+    
+    private lazy var hostingController: NSViewController = {
+        return NSHostingController(rootView: ContentView())
+    }()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        Task { @MainActor in
+            self.window.center()
+            self.window.makeKeyAndOrderFront(nil)
+        }
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(
+        _ sender: NSApplication
+    ) -> Bool {
+        return true
+    }
+    
+}
 
-    func applicationDidFinishLaunching(_ notification: Notification) {}
-
+fileprivate struct ContentView: View {
+    fileprivate var body: some View {
+        Text("Hello from InterposeKit!")
+            .font(.title)
+            .fixedSize()
+            .padding(200)
+    }
 }
